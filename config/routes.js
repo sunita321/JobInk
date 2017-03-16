fb2562e14cb93699ce68e1b3dbd8eb8fffed3b2a
Sunita Ramnarinesingh <sramnari@gmail.com>
2017-03-16 15:35:30 -0400

    Added routes and handlebar pages

---
+++ config/routes.js
@@ -0,0 +1,13 @@
+module.exports = function(router)
+{
+	router.get("/", function(req, res)
+	{
+		res.render("home");
+	});
+
+	router.get("/saved", function(req, res)
+	{
+		res.render("saved");
+	});
+
+}
---
+++ server.js
@@ -0,0 +1,55 @@
+//Dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+var hbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+
+// Set up Port
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+//Set Express Router
+var router = express.Router();
+
+//Require Routes
+require("./config/routes")(router);
+
+//Make public folder static
+app.use(express.static(__dirname + "/public"));
+
+//Connect Handlebars to Express
+app.engine("handlebars", hbs({
+	defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+//Use bodyParser
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.use(router);
+
+//Mongoose database
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoJobs";
+
+//Connnect mongoose to database
+mongoose.connect(db, function(error)
+{
+	if (error)
+	{
+		console.log(error);
+	}
+	else {
+		console.log("Mongoose connection successful!");
+	}
+});
+
+app.listen(PORT, function()
+{
+  console.log('Listening on port: ' + PORT);
+});
+
