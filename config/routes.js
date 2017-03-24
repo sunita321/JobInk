5e1e67f44aec83bde93ef1c87803b788ada0e72f
Sunita Ramnarinesingh <sramnari@gmail.com>
2017-03-23 23:05:03 -0400

    adding Auth0

--- config/routes.js
+++ config/routes.js
@@ -3,10 +3,24 @@ var getjobs = require("../scripts/getjobs");
 var indeedController = require("../controllers/indeed");
 var notesController = require("../controllers/notes");
 
+var passport = require('passport');
+
+var env = {
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+};
+
 
 
 module.exports = function(router)
 {
+		/* GET home page. */
+	router.get('/auth0', function(req, res, next) {
+	  res.render('index', { title: 'Express', env: env });
+	});
+
+
 	router.get("/", function(req, res)
 	{
 		res.render("home");
@@ -16,6 +30,23 @@ module.exports = function(router)
 	{
 		res.render("saved");
 	});
+
+	router.get('/login',
+	  function(req, res){
+	    res.render('login', { env: env });
+	  });
+
+	router.get('/logout', function(req, res){
+	  req.logout();
+	  res.redirect('/');
+	});
+
+	router.get('/callback',
+	  passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
+	  function(req, res) {
+	    res.redirect(req.session.returnTo || '/user');
+	  });
+
 
 	router.get("/api/fetch", function(req, res)
 	{
--- server.js
+++ server.js
@@ -6,6 +6,21 @@ var bodyParser = require("body-parser");
 var Promise = require('bluebird');
 mongoose.Promise = Promise;
 
+var passport = require('passport');//Adding Auth0
+var Auth0Strategy = require('passport-auth0');
+
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var dotenv = require('dotenv');
+var path = require('path');
+
+
+
+dotenv.load();
+
+var user = require('./config/user');
 
 // Set up Port
 var PORT = process.env.PORT || 3000;
@@ -20,18 +35,77 @@ var router = express.Router();
 require("./config/routes")(router);
 
 //Make public folder static
-app.use(express.static(__dirname + "/public"));
+//app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Connect Handlebars to Express
 app.engine("handlebars", hbs({
-	defaultLayout: "main"
+defaultLayout: "main"
 }));
-app.set("view engine", "handlebars");
 
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+//Auth0 strategy
+// Configure Passport to use Auth0
+var strategy = new Auth0Strategy({
+    domain:       process.env.AUTH0_DOMAIN,
+    clientID:     process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  }, function(accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  });
+
+passport.use(strategy);
+
+
+// This can be used to keep a smaller payload
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
 //Use bodyParser
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'shhhhhhhhh',
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+
+
+
+
+//Auth0 Config Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+app.use('/user', user);
+
+
+
+
+//Mongo
 
 app.use(router);
 
@@ -58,3 +132,4 @@ app.listen(PORT, function()
   console.log('Listening on port: ' + PORT);
 });
 
+module.exports = app;
